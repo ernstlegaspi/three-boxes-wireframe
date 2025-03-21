44cbe9daea5696c85425e39eb259311c71198e2f
Akie <legaspiernstdanielle@gmail.com>
2025-03-21 21:07:22 +0800

    Scene is pitch black, light follow cursor

--- app/(canvas)/CanvasPage.jsx
+++ app/(canvas)/CanvasPage.jsx
@@ -3,17 +3,17 @@
 import { OrbitControls, PerspectiveCamera } from "@react-three/drei"
 import { Canvas } from "@react-three/fiber"
 import Boxes from "./Boxes"
+import Lights from "./Light"
 
 export default function CanvasPage() {
 	return <>
 		<Canvas shadows>
 			<PerspectiveCamera makeDefault position={[0, 0, 5]} />
 
-			<ambientLight  intensity={1} />
-
 			<Boxes />
+			<Lights />
 
-			<OrbitControls enableDamping dampingFactor={.03} />
+			<OrbitControls enableRotate={false} enableDamping dampingFactor={.03} />
 		</Canvas>
 	</>
 }
--- app/(canvas)/Light.jsx
+++ app/(canvas)/Light.jsx
@@ -1,3 +1,29 @@
-export default function Light() {
-	return <></>
+import { useFrame } from "@react-three/fiber"
+import { useEffect, useRef, useState } from "react"
+
+export default function Lights() {
+	const [pointer, setPointer] = useState({ x: 0, y: 0 })
+	const ref = useRef()
+	
+	useEffect(() => {
+		const pointerMove = e => {
+			setPointer({
+				x: (e.clientX / window.innerWidth) * 2 - 1,
+				y: -(e.clientY / window.innerHeight) * 2 + 1
+			})
+		}
+
+		window.addEventListener("pointermove", pointerMove)
+
+		return () => {
+			window.removeEventListener("pointermove", pointerMove)
+		}
+	}, [])
+	
+	
+	useFrame(() => {
+		ref.current.position.set(pointer.x * 5, pointer.y * 5, 1)
+	})
+
+	return <pointLight ref={ref} distance={10} />
 }
