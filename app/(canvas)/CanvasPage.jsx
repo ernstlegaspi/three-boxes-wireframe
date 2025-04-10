79f1a4cba66907e4358cabdafa8cb624631c063a
Akie <legaspiernstdanielle@gmail.com>
2025-04-10 19:35:50 +0800

    Added text reminder

--- app/(canvas)/CanvasPage.jsx
+++ app/(canvas)/CanvasPage.jsx
@@ -2,9 +2,11 @@
 
 import { OrbitControls, PerspectiveCamera } from "@react-three/drei"
 import { Canvas } from "@react-three/fiber"
-import Boxes from "./Boxes"
-import Lights from "./Light"
 import { useEffect, useState } from "react"
+
+import Lights from "./Light"
+import Boxes from "./Boxes"
+import gsap from "gsap"
 
 export default function CanvasPage() {
 	const [innerWidth, setInnerWidth] = useState(0)
@@ -13,6 +15,17 @@ export default function CanvasPage() {
 		if(!window) return
 
 		setInnerWidth(window.innerWidth)
+
+		const interval = setInterval(() => {
+			gsap.to(document.getElementById("text"), {
+				opacity: "0",
+				duration: .3
+			})
+		}, 5000)
+
+		return () => {
+			clearInterval(interval)
+		}
 	}, [])
 
 	if(innerWidth < 1024) return <div className="flex items-center justify-center px-4 text-center text-white h-[100vh] w">
@@ -20,7 +33,9 @@ export default function CanvasPage() {
 		<p>Please use a desktop for the best experience.</p>
 	</div>
 	
-	return <>
+	return <div className="relative w-full h-full">
+		<div id="text" className="absolute text-white bottom-5 left-1/2 -translate-x-1/2 text-[25px] font-medium">Try moving your mouse</div>
+
 		<Canvas shadows>
 			<PerspectiveCamera makeDefault position={[0, 0, 5]} />
 
@@ -29,5 +44,5 @@ export default function CanvasPage() {
 
 			<OrbitControls enableRotate={false} enableDamping dampingFactor={.03} />
 		</Canvas>
-	</>
+	</div>
 }
